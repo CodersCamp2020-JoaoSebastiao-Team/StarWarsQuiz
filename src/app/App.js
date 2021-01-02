05f1b7d519835359f7d814e42b8d61c93c490032
Magdalena <sochamagdalena8@gmail.com>
2021-01-02 20:48:45 +0100

    handleModeUpdate
    Dodana funkcja handleModeUpdate, ktora pokazuje wybray tryb gry

--- src/app/App.js
+++ src/app/App.js
@@ -1,2 +1,16 @@
 export const App = ({options}) => {
 }
+
+const listItems = document.querySelectorAll('.menu-option');
+
+listItems.forEach(item => item.addEventListener('click', handleModeUpdate));
+
+function handleModeUpdate(e){
+    if(!e.target.classList.contains('selected')) {
+        for (var i = 0; i < listItems.length; i++) {
+            listItems[i].classList.remove('selected');
+        }
+        e.target.classList.add('selected');
+        console.log(e.target);   
+    }
+}
