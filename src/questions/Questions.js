fd2ebfc1379c2e30ad57ec435d23a488d1ecb77b
Kotlinski95 <kotlinski95@gmail.com>
2021-01-02 18:30:26 +0100

    SelectQuestion logic added - added answers and image changing according to right answer

--- src/questions/Questions.js
+++ src/questions/Questions.js
@@ -6,10 +6,15 @@ export const Questions = async (APIurl, category) => {
         default:
             break;
     }
+
+    let picture = document.getElementsByClassName('question-image__image')[0];
+    let options = document.getElementsByClassName('p-content--item');
+
     var StarWarsData;
     var QuestionsPeople = [];
     let fetchData = [];
     let nextUrl;
+    let selected = [];
 
     console.log("Inside Questions, API url: ", APIurl)
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
@@ -40,14 +45,26 @@ export const Questions = async (APIurl, category) => {
         getData(nextUrl);
     }
     console.log("Fetched data :", fetchData);
-    console.log("Fetched data length: ",fetchData.length)
+    console.log("Fetched data length: ", fetchData.length)
     const result = await waitForData();
     console.log(result);
-        for (const names in fetchData) {
-            QuestionsPeople[names] = fetchData[names].name
-        }
-        console.log("Fetched names of people :", QuestionsPeople);
-
+    for (const names in fetchData) {
+        QuestionsPeople[names] = fetchData[names].name
+    }
+    console.log("Fetched names of people :", QuestionsPeople);
+    showQuestion();
+    options[0].addEventListener("click", function () {
+        showQuestion();
+    });
+    options[1].addEventListener("click", function () {
+        showQuestion();
+    });
+    options[2].addEventListener("click", function () {
+        showQuestion();
+    });
+    options[3].addEventListener("click", function () {
+        showQuestion();
+    });
 
     async function getData(url) {
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
@@ -64,10 +81,75 @@ export const Questions = async (APIurl, category) => {
 
     function waitForData() {
         return new Promise(resolve => {
-                setTimeout(() => {
-                    resolve('resolved');
-                }, 3000);         
+            setTimeout(() => {
+                resolve('resolved');
+            }, 3000);
         });
+    }
+
+    function selectQuestion(questions, selected) {
+        let dubbled = false;
+        let selectedQuestion;
+        let selectedChoises = [];
+        let optionsSelected = false;
+        let max = questions.length;
+        let min = 0;
+        if (questions.length > selected.length) {
+            do {
+                const randomQuestion = Math.floor(Math.random() * (max - min + 1) + min);
+                dubbled = false;
+                for (let number of selected) {
+                    if (number == randomQuestion) {
+                        console.log("Dubbled!")
+                        dubbled = true;
+                    }
+                }
+                if (!dubbled) {
+                    selected.push(randomQuestion);
+                    selectedQuestion = randomQuestion;
+                }
+            }
+            while (dubbled)
+            do {
+                optionsSelected = false;
+                const randomChoise1 = Math.floor(Math.random() * (max - min + 1) + min);
+                const randomChoise2 = Math.floor(Math.random() * (max - min + 1) + min);
+                const randomChoise3 = Math.floor(Math.random() * (max - min + 1) + min);
+                if (randomChoise1 != randomChoise2 != randomChoise3 != selectedQuestion) {
+                    optionsSelected = true;
+                    selectedChoises.push(randomChoise1);
+                    selectedChoises.push(randomChoise2);
+                    selectedChoises.push(randomChoise3);
+                }
+            }
+            while (!optionsSelected)
+
+            let obj = {
+                good: selectedQuestion,
+                bad: [selectedChoises[0], selectedChoises[1], selectedChoises[2]]
+            }
+            return obj;
+        }
+        else{
+            return -1;
+        }
+    }
+
+    function showQuestion() {
+        let Answers = selectQuestion(QuestionsPeople, selected);
+        if (Answers != -1){
+            console.log("Good anser is nr : ", Answers.good)
+            picture.style.backgroundImage = `url(../static/assets/img/modes/${category}/${Answers.good + 1}.jpg)`;
+            console.log("Bad choises: ", Answers.bad[0], Answers.bad[1], Answers.bad[2]);
+            options[0].innerText = QuestionsPeople[Answers.good];
+            options[1].innerText = QuestionsPeople[Answers.bad[0]];
+            options[2].innerText = QuestionsPeople[Answers.bad[1]];
+            options[3].innerText = QuestionsPeople[Answers.bad[2]];
+        }
+        else{
+            console.log("You answered to all questions!!")
+        }
+
     }
 }
 
