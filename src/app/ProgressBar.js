e114fc6959577bc36623b0ef902cb73ad4d52afb
Mateusz Murat <mateuszdanielmurat@gmail.com>
2021-01-03 20:43:25 +0100

    Create countdown timer + improve progress bar animation and rearrange code

---
+++ src/app/ProgressBar.js
@@ -0,0 +1,35 @@
+export const ProgressBar = () => {
+
+  const timeLimit = 120;
+  let timePassed = 0;
+  let timeLeft = timeLimit;
+
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60) > 9 ? `${Math.floor(time / 60)}` : `0${Math.floor(time / 60)}`;
+    let seconds = time % 60 > 9 ? `${time % 60}` : `0${time % 60}`;
+    return `${minutes}:${seconds}`;
+  }
+
+  const startTimer = () => {
+    const timerInterval = setInterval(() => {
+      timePassed += 1;
+      timeLeft = timeLimit - timePassed;
+      document.querySelector('.timer>span').innerHTML = `Time left: ${formatTime(timeLeft)}`;
+      if (timeLeft === 0) {
+        clearInterval(timerInterval);
+      }
+    }, 1000)
+  }
+
+  document.querySelector('#progress-bar').innerHTML =
+     `<div class="progress-bar-wrapper">
+        <figure class="lightsaber-handle"><img src="static/assets/ui/LightsaberHandle.png" alt=""></figure>
+        <div class="lightsaber-empty">
+          <div class="lightsaber-full"></div>
+        </div>
+      </div>
+      <div class="timer">
+        <span>Time left: ${formatTime(timeLeft)}</span>
+      </div>`
+  startTimer();
+}
