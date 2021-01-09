f32e3d21cd226f6141d2e7cb483d10c5f2137708
Magdalena <sochamagdalena8@gmail.com>
2021-01-09 13:20:09 +0100

    unique class names

--- src/app/main-menu.js
+++ src/app/main-menu.js
@@ -4,11 +4,11 @@ const listItems = document.querySelectorAll('.main-menu--option');
 listItems.forEach(item => item.addEventListener('click', handleModeUpdate));
 
 export function handleModeUpdate(e){
-    if(!e.target.classList.contains('selected')) {
+    if(!e.target.classList.contains('main-menu--selected')) {
         for (var i = 0; i < listItems.length; i++) {
-            listItems[i].classList.remove('selected');
+            listItems[i].classList.remove('main-menu--selected');
         }
-        e.target.classList.add('selected');
+        e.target.classList.add('main-menu--selected');
         console.log(e.target);   
     }
 }
