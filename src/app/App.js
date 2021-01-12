85079f3315123487bafe7a223611354b1caa7deb
HubertSiewior <hubert-4321@wp.pl>
2021-01-12 23:23:35 +0100

    zasady gry update

--- src/app/App.js
+++ src/app/App.js
@@ -3,8 +3,10 @@ import {handleModeUpdate, category} from "./MainMenu";
 import {handleRulesButtonClick} from "./rankingAndMode"
 export const App = ({options}) => {
   const start = document.getElementsByClassName('play-the-game')[0];
+  const rules = document.getElementById("zasady_gry");
   start.addEventListener("click", () => {
     Questions(options.swApiBaseUrl,`${category}`);
+    rules.style.display="none";
   })
 
   const rulesRankingButton = document.querySelector('.hall-of-fame');//'.sw-quiz-mode-button-rules'
