acbb1c4a93a3b253aa1c458a07acfd641d5883ef
tomsm <tomsmo007@gmail.com>
2021-01-17 18:26:05 +0100

    Add ranking modal view after saving person to ranking.

--- src/app/rankingAndMode.js
+++ src/app/rankingAndMode.js
@@ -72,38 +72,44 @@ export function updateText(category) {
       ranking.filter((e, i) => i < 3).map((person, id) => {
         const i = id + 1;
         return '<tr><td>' + i + '</td><td>' + person.name + '</td> <td>' + person.score + '/' + person.max_score +
-          '</td><td>'+person.scorePercents+'%'+'</td></tr>';
+          '</td><td>'+person.scorePercents.toFixed(2)+'%'+'</td></tr>';
       }) + '</table><div class="all-ranking-btn-flex"><button id="all-ranking-btn">See all</button></div></div>' :
       '<div><h2>Mode ranking:</h2><p>The leadership is empty</p></div>';
 
   if (rulesRankingButton.textContent !== 'Hall of fame' && ranking.length) {
-    handleAllRanking(ranking)
+    handleAllRankingButton(ranking)
   }
 }
-function handleAllRanking(rankingArray) {
-  let modal = document.getElementById('Hall-of-fame-modal');
-  let seeAllButton = document.getElementById('all-ranking-btn');
-  let span = document.getElementsByClassName('close')[0];
-  seeAllButton.addEventListener('click', () => {
-    let rankingModalBody = document.querySelector('.modal-body');
-    rankingModalBody.innerHTML = '<div><table><tr><th>Place</th><th>Player</th><th>Answered</th><th>Percents</th></tr>' +
-      rankingArray.map((person, id) => {
-        const i = id + 1;
-        return '<tr><td>' + i + '</td><td>' + person.name + '</td> <td>' + person.score + '/' + person.max_score +
-          '</td><td>'+person.scorePercents+'%'+'</td></tr>';
-      }) + '</table></div>';
-    modal.style.display = 'block';
+const modal = document.getElementById('Hall-of-fame-modal');
+let span = document.getElementsByClassName('close')[0];
+span.addEventListener('click', () => modal.style.display = 'none');
+
+window.addEventListener('click',
+  (e) => {
+    if (e.target === modal) {
+      modal.style.display = 'none';
+    }
   });
-  span.addEventListener('click', () => modal.style.display = 'none');
-  window.addEventListener('click',
-    (e) => {
-      if (e.target === modal) {
-        modal.style.display = 'none';
-      }
-    });
+
+function handleAllRankingButton(rankingArray) {
+  let seeAllButton = document.getElementById('all-ranking-btn');
+  seeAllButton.addEventListener('click', () => {
+    modalRankingView(rankingArray)
+  });
 }
 
-const MAX_HIGH_SCORES = 5;
+
+function modalRankingView(rankingArray){
+  let rankingModalBody = document.querySelector('.modal-body');
+  rankingModalBody.innerHTML = '<div><table><tr><th>Place</th><th>Player</th><th>Answered</th><th>Percents</th></tr>' +
+    rankingArray.map((person, id) => {
+      const i = id + 1;
+      return '<tr><td>' + i + '</td><td>' + person.name + '</td> <td>' + person.score + '/' + person.max_score +
+        '</td><td>'+person.scorePercents.toFixed(2)+'%'+'</td></tr>';
+    }) + '</table></div>';
+  modal.style.display = 'block';
+}
+
 
 const username = document.getElementById("player-name-hall-of-fame");
 //const mostRecentScore = localStorage.getItem('mostRecentScore');
@@ -117,28 +123,31 @@ const highScoresVehicle = JSON.parse(localStorage.getItem(`highScoresVehicle`)) 
 const highScoresStarship = JSON.parse(localStorage.getItem(`highScoresStarship`)) || [];
 
 export function saveHighScore (e) {
-
+  e.preventDefault();
     const lastScore = {
         score: localStorage.getItem('mostRecentScore'),
         max_score: localStorage.getItem('QuestionsTotal'),
         scorePercents : (localStorage.getItem('mostRecentScore') / localStorage.getItem('QuestionsTotal')) * 100,
         name: username.value,
     };
+    let endSection = document.querySelector(".end-game")
+    let rulesSection = document.querySelector("#zasady_gry")
     switch(category) {
       case "people":
         highScoresPeople.push(lastScore);
         highScoresPeople.sort((a,b) => {
           return b.scorePercents - a.scorePercents;
         });
-        highScoresPeople.splice(MAX_HIGH_SCORES);
         localStorage.setItem("highScoresPeople", JSON.stringify(highScoresPeople));
+        endSection.style.display = "none";
+        rulesSection.style.display = "inline";
+        modalRankingView(highScoresPeople)
         break;
       case "vehicles":
         highScoresVehicle.push(lastScore);
         highScoresVehicle.sort((a,b) => {
           return b.scorePercents - a.scorePercents;
         });
-        highScoresVehicle.splice(MAX_HIGH_SCORES);
         localStorage.setItem("highScoresVehicle", JSON.stringify(highScoresVehicle));
         break;
       case "starships":
@@ -146,7 +155,6 @@ export function saveHighScore (e) {
         highScoresStarship.sort((a,b) => {
           return b.scorePercents - a.scorePercents;
         });
-        highScoresStarship.splice(MAX_HIGH_SCORES);
         localStorage.setItem("highScoresStarship", JSON.stringify(highScoresStarship));
     }
 }
