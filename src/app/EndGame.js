5b55531abd2fe24068bf8bb38781e4b27b02a29f
Magdalena <sochamagdalena8@gmail.com>
2021-01-17 17:28:26 +0000

    save button style

--- src/app/EndGame.js
+++ src/app/EndGame.js
@@ -2,7 +2,7 @@ const endGameScore = document.getElementById('end-result-p');
 const endGameSummary = document.getElementById('end-summary');
 export const EndGame = (Answers,score, computerScore) => { 
     endGameScore.innerText = `YOUR RESULT: ${score}`;
-    endGameSummary.innerText = `The force is strong in you young Padawan! During 2 minutes you have answered ${score} / ${Answers.length} questions. And Computer guessed ${computerScore} / ${Answers.length} .`;
+    endGameSummary.innerText = `The force is strong in you young Padawan! During 2 minutes you have answered ${score} / ${Answers.length} questions and Computer guessed ${computerScore} / ${Answers.length} .`;
     //console.log(Answers);
 
 }
